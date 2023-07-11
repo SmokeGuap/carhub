e00040ac8a942367fbccb12f4f259806c0d13f22
Kirill Ryzhkov <mustang.horse34@gmail.com>
2023-07-11 23:56:05 +0700

    Some fixes

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 const Hero = () => {
   const handleScroll = () => {};
   return (
-    <div className='flex xl:flex-row flex-col gap-5 relative z-0 max-w-[1440px] mx-auto'>
+    <div className='flex xl:flex-row flex-col gap-5 relative z-0 mx-auto overflow-hidden'>
       <div className='flex-1 pt-36 sm:px-16 px-6'>
         <h1 className='2xl:text-[72px] sm:text-[64px] text-[50px] font-extrabold'>
           Find or rent a car - quickly and easily!
@@ -24,7 +24,7 @@ const Hero = () => {
         <div className='relative xl:w-full w-[90%] xl:h-full h-[590px] z-0'>
           <Image src='/hero.png' alt='hero' fill className='object-contain' />
         </div>
-        <div className='absolute xl:-top-24 xl:-right-1/2 -right-1/4 bg-hero-bg bg-repeat-round -z-10 w-full xl:h-screen h-[590px] overflow-hidden'></div>
+        <div className='absolute xl:-top-24 xl:-right-1/3 -right-1/4 bg-hero-bg bg-repeat-round -z-10 w-full xl:h-screen h-[590px]'></div>
       </div>
     </div>
   );
