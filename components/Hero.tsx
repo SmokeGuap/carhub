4046b7615f5857e0b205f5d03de5e782e0d5a91d
Kirill Ryzhkov <mustang.horse34@gmail.com>
2023-07-11 23:34:54 +0700

    Add Footer

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -10,7 +10,7 @@ const Hero = () => {
         <h1 className='2xl:text-[72px] sm:text-[64px] text-[50px] font-extrabold'>
           Find or rent a car - quickly and easily!
         </h1>
-        <p className='text-[27px] text-black-100 font-light mt-5'>
+        <p className='text-[27px] font-light mt-5'>
           Streamline your car rental experience with our effortless booking
           process
         </p>
@@ -19,12 +19,12 @@ const Hero = () => {
           containerStyles='bg-main text-white rounded-full mt-10'
           handleClick={handleScroll}
         />
-        <div className='xl:flex-[1.5] flex justify-end items-end w-full xl:h-screen'>
-          <div className='relative xl:w-full w-[90%] xl:h-full h-[590px] z-0'>
-            <Image src='/hero.png' alt='hero' fill className='object-contain' />
-            <div className='absolute xl:-top-24 xl:-right-1/2 -right-1/4 bg-hero-bg bg-repeat-round -z-10 w-full xl:h-screen h-[590px] overflow-hidden'></div>
-          </div>
+      </div>
+      <div className='xl:flex-[1.5] flex justify-end items-end w-full xl:h-screen'>
+        <div className='relative xl:w-full w-[90%] xl:h-full h-[590px] z-0'>
+          <Image src='/hero.png' alt='hero' fill className='object-contain' />
         </div>
+        <div className='absolute xl:-top-24 xl:-right-1/2 -right-1/4 bg-hero-bg bg-repeat-round -z-10 w-full xl:h-screen h-[590px] overflow-hidden'></div>
       </div>
     </div>
   );
